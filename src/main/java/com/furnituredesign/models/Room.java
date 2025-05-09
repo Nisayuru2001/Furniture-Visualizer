9d2dacde07b2a07a3edff0d0ab0cb95a6aaa231a
Nisayuru Sananeth <120470493+Nisayuru2001@users.noreply.github.com>
2025-05-09 14:47:03 +0530

    room

--- src/main/java/com/furnituredesign/models/Room.java
+++ src/main/java/com/furnituredesign/models/Room.java
@@ -109,7 +109,18 @@ public class Room {
         }
     }
 
+    // Getters and setters
+    public double getWidth() {
+        return width;
+    }
 
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
 
     public void setLength(double length) {
         this.length = length;
